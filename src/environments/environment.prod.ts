6d025f0a498e3272a6eebb0a347a279849ec3dd3
sanketshivale <shivalesanket21@gmail.com>
2024-01-26 01:01:46 +0530

    UPdates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'https://angular-app-server-production.up.railway.app/api',
+  serverBaseUrl: 'https://angular-app-server-production.up.railway.app',
 };
